4cd6c4973a10629c9364d0e557289a64afcad6e9
Rakhmatullo929 <tillo3305@gmail.com>
2022-07-08 06:09:24 +0300

    masterpush

--- config/hotel/models.py
+++ config/hotel/models.py
@@ -1,28 +1,35 @@
+# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import SET_NULL
 
 
 # Create your models here.
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    created_by = models.ForeignKey(User, SET_NULL, null=True, blank=True,
+                                   related_name='created_%(model_name)ss')
+    updated_by = models.ForeignKey(User, SET_NULL, null=True, blank=True,
+                                   related_name='updated_%(model_name)ss')
 
     class Meta:
         abstract = True
 
 
-class Room(models.Model):
+class Room(BaseModel):
     number = models.IntegerField(null=True)
     image = models.ImageField()
     description = models.TextField()
-    type = models.ForeignKey('hotel.Type', on_delete=models.CASCADE)
+    type = models.ForeignKey('Type', related_name='Type', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'Room - {self.number}'
 
 
-class Type(models.Model):
+class Type(BaseModel):
     title = models.CharField(max_length=255)
 
     def __str__(self):
